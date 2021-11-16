7f9555296f10386ec27ebbbed4ba9d54fc5d5b7c
donelli <49295044+donelli@users.noreply.github.com>
2021-11-16 17:28:40 -0300

    implemented position functions

--- pkg/shared/position.go
+++ pkg/shared/position.go
@@ -7,8 +7,29 @@ import (
 type Position struct {
 	Row    int32
 	Column int32
+	Index  int32
 }
 
-func (p Position) String() string {
+func (p *Position) String() string {
 	return fmt.Sprintf("(%d,%d)", p.Row, p.Column)
+}
+
+func (p *Position) Advance(char rune) {
+
+	p.Index++
+	p.Column++
+
+	if char == '\n' {
+		p.Row++
+		p.Column = 0
+	}
+
+}
+
+func NewPosition() *Position {
+	return &Position{
+		Row:    0,
+		Column: -1,
+		Index:  -1,
+	}
 }
