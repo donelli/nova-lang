7aac18e3cb7ec5a52b93848d364ffa1bdbc2d497
donelli <49295044+donelli@users.noreply.github.com>
2021-11-16 17:29:07 -0300

    lexer advance method and lexer tests

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -2,14 +2,23 @@ package lexer
 
 import (
 	"errors"
+	"recital_lsp/pkg/shared"
 )
 
 type Lexer struct {
 	FileName    string
 	FileContent string
+
+	CurrentPosition *shared.Position
+	CurrentChar     rune
 }
 
 type LexerResult struct {
+}
+
+func (lexer *Lexer) Advance() {
+	lexer.CurrentPosition.Advance(lexer.CurrentChar)
+	lexer.CurrentChar = rune(lexer.FileContent[lexer.CurrentPosition.Index])
 }
 
 func (lexer *Lexer) Parse() (LexerResult, error) {
@@ -18,7 +27,9 @@ func (lexer *Lexer) Parse() (LexerResult, error) {
 
 func NewLexer(fileName string, fileContent string) *Lexer {
 	return &Lexer{
-		FileName:    fileName,
-		FileContent: fileContent,
+		FileName:        fileName,
+		FileContent:     fileContent,
+		CurrentChar:     ' ',
+		CurrentPosition: shared.NewPosition(),
 	}
 }
