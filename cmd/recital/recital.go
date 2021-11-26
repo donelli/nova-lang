f97f80c578c48537cf12c244868a61fd0f0a6c2e
donelli <eduardopellenz@hotmail.com>
2021-11-25 23:18:59 -0300

    new line normalization and better print in lex command

--- cmd/recital/recital.go
+++ cmd/recital/recital.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"recital_lsp/pkg/lexer"
+	"recital_lsp/pkg/utils"
+	"time"
 )
 
 func main() {
@@ -22,21 +24,34 @@ func main() {
 			return
 		}
 
+		fileReadStart := time.Now()
+
 		dat, err := os.ReadFile(os.Args[2])
+
+		fileReadEnd := time.Now()
 
 		if err != nil {
 			fmt.Printf("%v\n", err)
 			return
 		}
 
+		dat = utils.NormalizeNewlines(dat)
+
+		lexerStartTime := time.Now()
+
 		lex := lexer.NewLexer(os.Args[2], string(dat))
 
 		res, err := lex.Parse()
+
+		lexerEndTime := time.Now()
 
 		if err != nil {
 			fmt.Printf("err: %v\n", err)
 			return
 		}
+
+		fmt.Printf("\n-> File read time: %d ms\n", (fileReadEnd.Sub(fileReadStart)).Milliseconds())
+		fmt.Printf("-> Lexer time: %d ms\n", (lexerEndTime.Sub(lexerStartTime)).Milliseconds())
 
 		if len(res.Errors) == 0 {
 			fmt.Printf("\n-> No errors\n")
