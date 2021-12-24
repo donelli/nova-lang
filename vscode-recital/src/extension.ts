6d48154c48f34325ef40cb461f8b776e52e3da56
donelli <eduardopellenz@hotmail.com>
2021-12-24 09:58:12 -0300

    created basic sintax and language configuration

--- vscode-recital/src/extension.ts
+++ vscode-recital/src/extension.ts
@@ -2,51 +2,64 @@ import { exec, execSync } from 'child_process';
 import { open, openSync, readFile, readSync } from 'fs';
 import * as vscode from 'vscode';
 
+let diagnosticCollection: vscode.DiagnosticCollection;
+
 export function activate(context: vscode.ExtensionContext) {
 	
-	let diagnosticCollection: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection('recital');;
+	diagnosticCollection = vscode.languages.createDiagnosticCollection('recital');
 	
-	let disposable = vscode.commands.registerCommand('donelli-recital.run', () => {
-		
-		const uri = vscode.window.activeTextEditor?.document.uri.fsPath;
-		
-		execSync(`E:\\Projects\\recital\\rt.exe parse ${uri} -json E:\\Projects\\recital\\out.json`);
-		
-		readFile('E:\\Projects\\recital\\out.json', 'utf-8' , (err, data) => {
-			if (err) {
-			  vscode.window.showErrorMessage(err.message);
-			  return;
-			}
-
-			const diagnostic = JSON.parse(data);
-			const diag = [];
-			
-			console.log(diagnostic);
-			
-			// diagnosticCollection.clear();
-			
-			for (let err of diagnostic.errors) {
-				let start = new vscode.Position(err.range.start.row, err.range.start.column);
-				let end = new vscode.Position(err.range.end.row, err.range.end.column);
-				diag.push(new vscode.Diagnostic(new vscode.Range(start, end), err.type + ' ' + err.message, vscode.DiagnosticSeverity.Error));
-			}
-
-			for (let warn of diagnostic.warnings) {
-				let start = new vscode.Position(warn.range.start.row, warn.range.start.column);
-				let end = new vscode.Position(warn.range.end.row, warn.range.end.column);
-				diag.push(new vscode.Diagnostic(new vscode.Range(start, end), warn.type + ' ' + warn.message, vscode.DiagnosticSeverity.Warning));
-			}
-
-			diagnosticCollection.set(vscode.window.activeTextEditor!.document.uri, diag);
-
-			console.log(diag);
-			
-		 });
-		
+	let disposableOpen = vscode.workspace.onDidOpenTextDocument(document => {
+		loadDiagnostics(document);
 	});
 
-	context.subscriptions.push(disposable, diagnosticCollection);
+	let disposableChange = vscode.workspace.onDidSaveTextDocument(document => {
+		loadDiagnostics(document);
+	});
+	
+	context.subscriptions.push(disposableOpen, disposableChange, diagnosticCollection);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function loadDiagnostics(doc: vscode.TextDocument) {
+
+	if (doc.languageId !== 'recital') {
+		return;
+	}
+	
+	diagnosticCollection.set(doc.uri, undefined);
+	
+	const uri = doc.uri.fsPath;
+	
+	execSync(`E:\\Projects\\recital\\rt.exe parse ${uri} -json E:\\Projects\\recital\\out.json`);
+	
+	readFile('E:\\Projects\\recital\\out.json', 'utf-8' , (err, data) => {
+		if (err) {
+			vscode.window.showErrorMessage(err.message);
+			return;
+		}
+
+		const diagnostic = JSON.parse(data);
+		const diag = [];
+		
+		console.log(diagnostic);
+		
+		for (let err of diagnostic.errors) {
+			let start = new vscode.Position(err.range.start.row, err.range.start.column);
+			let end = new vscode.Position(err.range.end.row, err.range.end.column);
+			diag.push(new vscode.Diagnostic(new vscode.Range(start, end), err.type + ' ' + err.message, vscode.DiagnosticSeverity.Error));
+		}
+
+		for (let warn of diagnostic.warnings) {
+			let start = new vscode.Position(warn.range.start.row, warn.range.start.column);
+			let end = new vscode.Position(warn.range.end.row, warn.range.end.column);
+			diag.push(new vscode.Diagnostic(new vscode.Range(start, end), warn.type + ' ' + warn.message, vscode.DiagnosticSeverity.Warning));
+		}
+
+		diagnosticCollection.set(doc.uri, diag);
+
+		console.log(diag);
+		
+		});
+	
+}
